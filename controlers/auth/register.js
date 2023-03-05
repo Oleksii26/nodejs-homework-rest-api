a99ef8502a5355c46d67c11f51fe6f3a3fb73fc4
Oleksii26 <Kolesnik26@ukr.net>
2023-03-05 11:08:42 +0200

    save

--- controlers/auth/register.js
+++ controlers/auth/register.js
@@ -2,6 +2,9 @@ const { Conflict } = require('http-errors')
 const bcrypt = require('bcryptjs')
 const { User } = require('../../models')
 const gravatar = require('gravatar')
+const { v4: uuidv4 } = require('uuid');
+
+const { sendEmail } = require('../../helpers')
 
 const register = async (req, res) => {
     const { name, email, password } = req.body
@@ -9,19 +12,29 @@ const register = async (req, res) => {
     if (user) {
         throw new Conflict(`User with ${email} already exist`)
     }
+    const verificationToken = uuidv4()
     const avatarURL = gravatar.url(email)
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    const result = await User.create({ name, email, password: hashPassword, avatarURL })
+    const result = await User.create({ name, email, password: hashPassword, avatarURL, verificationToken })
+
+    const mail = {
+        to: email,
+        subject: 'Підтвердження електронної адреси',
+        html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Натисніть для підтвердження електронної адреси</a>`
+    }
+    await sendEmail(mail)
+
     res.status(201).json({
         status: 'success',
         code: 201,
         data: {
             user: {
                 name,
-                email, 
-                avatarURL
-            } 
-        } 
+                email,
+                avatarURL,
+                verificationToken
+            }
+        }
     })
 }
 
